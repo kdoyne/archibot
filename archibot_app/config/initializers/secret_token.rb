74958d6d889c3f7c2ed17ee59fcf86476d30461d
kdoyne <kdoyne930@gmail.com>
2014-02-14 09:33:34 -0500

    first project commit

---
+++ archibot_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArchibotApp::Application.config.secret_key_base = '410be01ada847ee3c2881f004916d159e038cf1c3534027a6e1399268a877c093ec14283bfd3d14c6118fdf230acadeb7a395403043595f8a42429a0595eee6c'
