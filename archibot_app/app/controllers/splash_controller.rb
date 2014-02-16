2ec5216d84e6344c8efb1a1c715fbce273383b14
kdoyne <kdoyne930@gmail.com>
2014-02-16 11:40:59 -0500

    instagram photos on main page

--- archibot_app/app/controllers/splash_controller.rb
+++ archibot_app/app/controllers/splash_controller.rb
@@ -1,4 +1,15 @@
 class SplashController < ApplicationController
   def index
+    @photos = get_photos
   end
+
+  private
+
+  def get_photos
+    tag = ["urbanlandscape","architecture","buildings","archidaily"].sample
+    return_hash = HTTParty.get("https://api.instagram.com/v1/tags/#{tag}/media/recent?client_id=#{INSTAGRAM_CLIENT_ID}")
+    @instagram_photos = [return_hash["data"].sample["images"]["low_resolution"]["url"], return_hash["data"].sample["images"]["low_resolution"]["url"],return_hash["data"].sample["images"]["low_resolution"]["url"],
+      return_hash["data"].sample["images"]["low_resolution"]["url"],return_hash["data"].sample["images"]["low_resolution"]["url"]]
+  end
+
 end
